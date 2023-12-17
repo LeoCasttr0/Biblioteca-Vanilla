2e5ccaa55578c466997dd42f428bc02f2cd7b853
LeoCasttr0 <142045008+LeoCasttr0@users.noreply.github.com>
2023-12-17 01:44:26 -0300

    status pegando

--- HTML/Aluguel.php
+++ HTML/Aluguel.php
@@ -374,20 +374,7 @@ $mostrarIcones = true;
         </div>
     </main>
 
-    <script>
-    document.addEventListener('DOMContentLoaded', function () {
-    function escondeIcones(el) {
-        var divElement = document.getElementById(el);
-        var editIcon = divElement.querySelector('.edit');
-        var trashIcon = divElement.querySelector('.trash');
-    
-        if (editIcon && trashIcon) {
-            editIcon.style.display = 'none';
-            trashIcon.style.display = 'none';
-        }
-    }
-});
-</script>
+
     
     <!--JS-->
  <!-- JS -->
